5fad4adee65c2e5b62a143030112565c84ddfd03
Himanshu7am-cyber <himanshupandey2561@gmail.com>
2024-07-30 17:56:33 +0530

    Add files via upload

---
+++ productdetails.js
@@ -0,0 +1,17 @@
+<script>
+var MenuItems=documents.getElementsById ="0px";
+
+MenuItems.style.maxHeight=="0px";
+
+function menutoggle()
+{
+	if( MenuItems.style.maxheight=="0px")
+	{
+	MenuItems.style.maxheight="200px"
+}
+else 
+{
+	MenuItems.style.maxheight="0px";
+}	
+}
+</script>
